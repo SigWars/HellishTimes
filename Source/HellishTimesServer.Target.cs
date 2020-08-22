ee9430cddbbeca9a5c8d6db09fd2d5351eba7e32
SigWar <highnetlan@gmail.com>
2020-08-22 00:15:36 -0300

    - Eletronic Nodes - Multiplayer Dedicated Server

---
+++ Source/HellishTimesServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class HellishTimesServerTarget : TargetRules
+{
+    public HellishTimesServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("HellishTimes");
+	}
+}
